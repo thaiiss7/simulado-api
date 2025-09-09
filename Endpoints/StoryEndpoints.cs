692bc67bdb70073d65e7b254b09eebd61cc50617
thaiiss7 <thaismichel7@gmail.com>
2025-09-09 00:19:14 -0300

    createstory endpoint

--- Endpoints/StoryEndpoints.cs
+++ Endpoints/StoryEndpoints.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using Simulado.UseCases.CreateStory;
+
 namespace Simulado.Endpoints;
 
 public static class StoryEndpoints
@@ -5,6 +8,17 @@ public static class StoryEndpoints
     public static void ConfigureStoryEndpoints(this WebApplication app)
     {
         //mappost para criar historia
+        app.MapPost("story", async (
+            [FromBody] CreateStoryRequest request,
+            [FromServices] CreateStoryUseCase useCase) => 
+            {
+                var result = await useCase.Do(request);
+                if (result.IsSuccess)
+                    return Results.Created(); 
+
+                return Results.BadRequest(result.Reason);
+            });
+
         //mapdelete para deletar historia
     }
 }
